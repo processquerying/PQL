8e696cbd9672c6509c5b56247edfa9c21c2916d8
Artem Polyvyanyy <processquerying@gmail.com>
2015-09-14 09:13:20 +1000

    Improvements to the documentation of IPQLBasicPredicatesOnTransitions interface.

--- src/org/pql/core/IPQLBasicPredicatesOnTransitions.java
+++ src/org/pql/core/IPQLBasicPredicatesOnTransitions.java
@@ -2,68 +2,96 @@ package org.pql.core;
 
 import org.jbpt.petri.IFlow;
 import org.jbpt.petri.IMarking;
+import org.jbpt.petri.INetSystem;
 import org.jbpt.petri.INode;
 import org.jbpt.petri.IPlace;
 import org.jbpt.petri.ITransition;
 import org.pql.logic.ThreeValuedLogicValue;
 
 /**
+ * Interface to PQL basic predicates (on Petri net transitions).
+ * 
  * @author Artem Polyvyanyy
  */
 public interface IPQLBasicPredicatesOnTransitions<F extends IFlow<N>, N extends INode, P extends IPlace, T extends ITransition, M extends IMarking<F,N,P,T>> {
 	
 	/**
-	 * Check if there is an execution in which a given transition occurs.
+	 * Check if {@link INetSystem} has an execution in which a given transition occurs.
+	 * 
 	 * @category core
-	 * @param t A transition.
-	 * @return <tt>TRUE</tt> if there is an execution in which a given transition occurs, 
-	 * <tt>FALSE</tt> if there is no execution in which a given transition occurs, and
-	 * <tt>NULL</tt> if there is no transition.
+	 * @param t Transition.
+	 * 
+	 * @return {@code UNKNOWN} if {@code t} is not a transition of {@link INetSystem}; otherwise
+	 * {@code TRUE} if {@link INetSystem} has an execution in which {@code t} occurs, or 
+	 * {@code FALSE} if {@link INetSystem} has no execution in which {@code t} occurs.
 	 */
 	public ThreeValuedLogicValue canOccur(T t);
 	
 	/**
-	 * Check if a given transition occurs in all executions.
+	 * Check if a given transition occurs in every execution of {@link INetSystem}.
+	 * 
 	 * @category core
-	 * @param t A transition.
-	 * @return <tt>TRUE</tt> if a given transition occurs in all executions,
-	 * <tt>FALSE</tt> if a given transition does not occur in all executions, and
-	 * <tt>NULL</tt> if there is no transition.
+	 * @param t Transition.
+	 * 
+	 * @return {@code UNKNOWN} if {@code t} is not a transition of {@link INetSystem}; otherwise
+	 * {@code TRUE} if {@code t} occurs in every execution of {@link INetSystem}, or 
+	 * {@code FALSE} if {@code t} does not occur in some execution of {@link INetSystem}.
 	 */
 	public ThreeValuedLogicValue alwaysOccurs(T t);
 	
 	/**
-	 * Given transitions t1 and t2, check if there is an execution in which t1 occurs and t2 does not occur.
-	 * @category 4C
-	 * @param t1 Transition t1.
-	 * @param t2 Transition t2.
-	 * @return <tt>TRUE</tt> if there is an execution in which t1 occurs and t2 does not occur,
-	 * <tt>FALSE</tt> if there is no execution in which t1 occurs and t2 does not occur, and
-	 * <tt>NULL</tt> if there is no transition t1 or t2.
+	 * Check if a given transition {@code t1} can conflict with a given transition {@code t2} in {@link INetSystem},
+	 * i.e., there is an execution in {@link INetSystem} in which {@code t1} occurs and {@code t2} does not.
+	 * 
+	 * @category 4C spectrum
+	 * @param t1 Transition.
+	 * @param t2 Transition.
+	 * 
+	 * @return {@code UNKNOWN} if either {@code t1} or {@code t2} is not a transition of {@link INetSystem}; otherwise
+	 * {@code TRUE} if {@code t1} can conflict with {@code t2} in {@link INetSystem}, or 
+	 * {@code FALSE} if {@code t1} cannot conflict with {@code t2} in {@link INetSystem}.
 	 */
 	public ThreeValuedLogicValue canConflict(T t1, T t2);
 	
 	/**
-	 * Check if there is an execution in which both given transitions occur.
-	 * @category 4C
-	 * @param t1 A transition.
-	 * @param t2 A transition.
-	 * @return <tt>TRUE</tt> if there is an execution in which both given transitions occur,
-	 * <tt>FALSE</tt> if there is no execution in which both given transitions occur, and
-	 * <tt>NULL</tt> if there is no transition t1 or t2.
+	 * Check if given transitions {@code t1} and {@code t2} can cooccur in {@link INetSystem},
+	 * i.e., there is an execution in {@link INetSystem} in which both {@code t1} and {@code t2} occur.
+	 * 
+	 * @category 4C spectrum
+	 * @param t1 Transition.
+	 * @param t2 Transition.
+	 * 
+	 * @return {@code UNKNOWN} if either {@code t1} or {@code t2} is not a transition of {@link INetSystem}; otherwise
+	 * {@code TRUE} if {@code t1} and {@code t2} can cooccur in {@link INetSystem}, or 
+	 * {@code FALSE} if {@code t1} and {@code t2} do not cooccur in {@link INetSystem}.
 	 */
 	public ThreeValuedLogicValue canCooccur(T t1, T t2);
 	
 	/**
-	 * Check if there is a total causal relation between two transitions of a net system.
-	 * @category 4C
-	 * @param t1 A transition.
-	 * @param t2 A transition.
-	 * @return <tt>TRUE</tt> if t1 is total casual with t2,
-	 * <tt>FALSE</tt> if t1 is NOT total casual with t2, and
-	 * <tt>NULL</tt> if either t1 or t2 is not a transition of the net system.
+	 * Check if a given transition {@code t1} is total causal with a given transition {@code t2} in {@link INetSystem},
+	 * i.e., in every concurrent execution of {@link INetSystem} it holds that {@code t1} is causal with {@code t2}.
+	 * 
+	 * @category 4C spectrum
+	 * @param t1 Transition.
+	 * @param t2 Transition.
+	 * 
+	 * @return {@code UNKNOWN} if either {@code t1} or {@code t2} is not a transition of {@link INetSystem}; otherwise
+	 * {@code TRUE} if {@code t1} is total causal with {@code t2} in {@link INetSystem}, or 
+	 * {@code FALSE} if {@code t1} is not total causal with {@code t2} in {@link INetSystem}.
 	 */
 	public ThreeValuedLogicValue totalCausal(T t1, T t2);
 	
+	/**
+	 * Check if given transitions {@code t1} and {@code t2} are total concurrent in {@link INetSystem},
+	 * i.e., in every concurrent execution of {@link INetSystem} it holds that {@code t1} and {@code t2} are concurrent.
+	 * 
+	 * @category 4C spectrum
+	 * @param t1 Transition.
+	 * @param t2 Transition.
+	 * 
+	 * @return {@code UNKNOWN} if either {@code t1} or {@code t2} is not a transition of {@link INetSystem}; otherwise
+	 * {@code TRUE} if {@code t1} and {@code t2} are concurrent in {@link INetSystem}, or 
+	 * {@code FALSE} if {@code t1} and {@code t2} are not concurrent in {@link INetSystem}.
+	 */
 	public ThreeValuedLogicValue totalConcur(T t1, T t2);
 }
