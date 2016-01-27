c051979b62dfec5dd04b2b9c2a848fbf89d544dc
artempolyvyanyy <artem.polyvyanyy@gmail.com>
2016-01-27 15:31:28 +1000

    Correcting indentations, adding missing library and licence files

--- src/org/pql/bot/AbstractPQLBot.java
+++ src/org/pql/bot/AbstractPQLBot.java
@@ -25,8 +25,8 @@ import org.pql.mc.IModelChecker;
  * @author Artem Polyvyanyy
  */
 public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlace, T extends ITransition, M extends IMarking<F,N,P,T>> 
-	extends Thread
-	implements IPQLBotHeartBeat {
+extends Thread
+implements IPQLBotHeartBeat {
 
 	/** The name of the logger is "org.pql.bot.[name]". */
 	private Logger logger;
@@ -49,9 +49,9 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 	protected String	PQL_INDEX_BOTS_IS_ALIVE		= "{? = CALL pql.pql_index_bots_is_alive(?)}";
 
 	/**
-         * @param botName  identifies this bot in log messages; cannot be the same as any other preexisting instance;
-         * @throws NameInUseException if <var>botName</var> is in use as the identifer for another instance of this class
-         */
+	 * @param botName  identifies this bot in log messages; cannot be the same as any other preexisting instance;
+	 * @throws NameInUseException if <var>botName</var> is in use as the identifer for another instance of this class
+	 */
 	public AbstractPQLBot(Connection con, String botName, IPQLIndex<F,N,P,T,M> index, IModelChecker<F,N,P,T,M> mc, IndexType indexType, long indexTime, long sleepTime) throws ClassNotFoundException, NameInUseException, SQLException {
 		this.connection = con;
 		this.botName	= (botName==null) ? UUID.randomUUID().toString() : botName;
@@ -64,51 +64,51 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 		if (this.isAlive(this.botName)) {
 			throw new NameInUseException(this.botName);
 		}
-		
+
 		this.logger = LoggerFactory.getLogger("org.pql.bot." + this.botName);
 
 		// TODO: ensure name is not hijacked between isAlive and alive
 		this.alive(this.botName);
-		
+
 		this.regularService = new PQLBotRegularServiceThread(this.botName, this.index, this);
 		this.regularService.start();
-		
+
 		this.isActive = true;
 	}
 
-        /**
-         * Report of an attempt to create a bot without a unique name.
-         */
+	/**
+	 * Report of an attempt to create a bot without a unique name.
+	 */
 	public static class NameInUseException extends Exception {
 		private final String name;
 		NameInUseException(String name) { this.name = name; }
 		String getName() { return name; }
-        }
-	
+	}
+
 	public boolean index(int modelID) throws SQLException, InterruptedException {
 		if (!this.isActive) return false;
-		
+
 		IndexStatus status = this.index.getIndexStatus(modelID);
-		
-                switch (status) {
+
+		switch (status) {
 		case UNINDEXED:
 			this.index.requestIndexing(modelID, this.botName);
-			
+
 			boolean start = this.index.startIndexing(modelID, this.botName);
 			if (start) {
 				indexThread = new PQLBotIndexThread(modelID,this.botName,this.MC,this.index,this.indexType);
 				indexThread.start();
-				
+
 				long startTime = System.currentTimeMillis();
 				while (indexThread.isAlive() && ((System.currentTimeMillis()-startTime) < (this.indexTime * 1000L))) {
 					Thread.sleep(1000L);
 				}
-        		
+
 				if (indexThread.isAlive()) {
 					indexThread.interrupt();
 					logger.warn(String.format("Interrupted job with ID %s because indexing took longer than %s seconds.", modelID, this.indexTime));
 				}
-        				                				
+
 				boolean result = indexThread.hasCompleted() && indexThread.getResult();
 				if (result) {
 					this.index.finishIndexing(modelID, this.botName);
@@ -120,7 +120,7 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 					this.index.deleteIndex(modelID);
 					return false;
 				}
-		      	}
+			}
 			else {
 				logger.warn(String.format("Failed to start indexing the model with ID %s.", modelID));
 				return false;
@@ -143,7 +143,7 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 	@Override
 	public void run() {
 		if (!this.isActive) return;
-		
+
 		for (;;) { // forever
 			try {
 				// get next index job
@@ -154,7 +154,7 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 					logger.debug(String.format("Retrieved indexing job for the model with ID %s.", modelID));
 					this.index(modelID);
 				}
-            	
+
 				// time to sleep before the next job
 				logger.debug(String.format("Going to sleep for %s seconds.", sleepTime));
 				Thread.sleep(sleepTime*1000);
@@ -165,65 +165,65 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 			}	
 		}
 	}
-	
+
 	class PQLBotRegularServiceThread extends Thread {
 		protected IPQLIndex<F,N,P,T,M>	index = null;
 		protected IPQLBotHeartBeat 			alive = null;
-		
+
 		protected String botName 	= null;
-		
+
 		PQLBotRegularServiceThread(String botName, IPQLIndex<F,N,P,T,M> index, IPQLBotHeartBeat alive) {
 			this.botName = botName;
 			this.index = index;
 			this.alive = alive;
 		}
 
-	    public void run() {
-	    	try {
-	    		for (;;) { // forever
-		    		// send alive message
-		    		try {
-		    			this.alive.alive(this.botName);
-					logger.debug("Sent an alive message.");
-				} catch (SQLException e) {
-					logger.error("Failed to send an alive message.", e);
-				}
-		    		
-		    		// sleep for 30 minutes
-		    		Thread.sleep(1800*1000);
-		    		
-		    		// cleanup index
-				try {
-					this.index.cleanupIndex();
-					logger.debug("Requested index cleanup.");
-				} catch (SQLException e) {
-					logger.error("Unable to request index cleanup.", e);
-				}
+		public void run() {
+			try {
+				for (;;) { // forever
+					// send alive message
+					try {
+						this.alive.alive(this.botName);
+						logger.debug("Sent an alive message.");
+					} catch (SQLException e) {
+						logger.error("Failed to send an alive message.", e);
+					}
 
-				// sleep for 30 minutes
-				Thread.sleep(1800*1000);
-		    	}
-	    	}
-	    	catch (InterruptedException e) {}
-	    }
+					// sleep for 30 minutes
+					Thread.sleep(1800*1000);
+
+					// cleanup index
+					try {
+						this.index.cleanupIndex();
+						logger.debug("Requested index cleanup.");
+					} catch (SQLException e) {
+						logger.error("Unable to request index cleanup.", e);
+					}
+
+					// sleep for 30 minutes
+					Thread.sleep(1800*1000);
+				}
+			}
+			catch (InterruptedException e) {}
+		}
 	};
 
 	class PQLBotIndexThread extends Thread {
 		protected IPQLIndex<F,N,P,T,M>	index = null;
 		protected IModelChecker<F,N,P,T,M> MC = null;
-		
+
 		protected String botName 	= null;
 		protected int modelID 		= 0;
-		
+
 		private boolean result = false;
 		private boolean completed = false;
-		
+
 		protected IndexType indexType	= null;
-		
+
 		PQLBotIndexThread(int modelID, String botName, IModelChecker<F,N,P,T,M> mc, IPQLIndex<F,N,P,T,M> index, IndexType indexType) {
 			this.botName = botName;
 			this.modelID = modelID;
-			
+
 			this.index		= index;
 			this.MC 		= mc;
 			this.indexType	= indexType;
@@ -236,11 +236,11 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 				logger.debug(String.format("Start checking model with ID %s.", this.modelID));
 				boolean check = this.index.checkNetSystem(modelID);
 				logger.debug(String.format("Finished checking model with ID %s.", this.modelID));
-        		
+
 				if (check) {
 					logger.info(String.format("Start indexing model with ID %s.", this.modelID));
 					this.result = this.index.constructIndex(modelID, this.indexType);
-        			
+
 					if (this.result) {
 						this.index.finishIndexing(this.modelID, this.botName);
 						logger.info(String.format("Finished indexing model with ID %s.", this.modelID));
@@ -258,7 +258,7 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 		public boolean getResult() {
 			return this.result;
 		}
-		
+
 		public boolean hasCompleted() {
 			return this.completed;
 		}
@@ -267,11 +267,11 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 	@Override
 	public void alive(String botName) throws SQLException {
 		if (botName == null || botName.isEmpty()) return;
-		
+
 		CallableStatement cs = connection.prepareCall(this.PQL_INDEX_BOTS_ALIVE);
-		
+
 		cs.setString(1, botName);
-		
+
 		cs.execute();
 		cs.close();
 	}
@@ -279,28 +279,28 @@ public class AbstractPQLBot<F extends IFlow<N>, N extends INode, P extends IPlac
 	@Override
 	public boolean isAlive(String botName) throws SQLException {
 		if (botName == null || botName.isEmpty()) return false;
-		
+
 		CallableStatement cs = connection.prepareCall(this.PQL_INDEX_BOTS_IS_ALIVE);
-		
+
 		cs.registerOutParameter(1, java.sql.Types.BOOLEAN);
 		cs.setString(2, botName);
-		
+
 		cs.execute();
-		
+
 		boolean result = cs.getBoolean(1);
-		
+
 		cs.close();
-		
+
 		return result;
 	}
 
 	public void terminate() {
 		if (this.regularService!=null)
 			this.regularService.interrupt();
-		
+
 		if (this.indexThread!=null)
 			this.indexThread.interrupt();
-		
+
 		this.interrupt();
 	}
 }
